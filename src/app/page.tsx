814eed52074a2e1f4638d400ff619b21577f1d75
snehasishkun <snehasishcodes@gmail.com>
2025-04-18 00:45:32 +0530

    fix: bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import ThemeToggle from "@/components/ui/theme-toggle";
 import { CardCVVInput, CardExpiryInput, CardNumberInput } from "@/components/credit-card-input";
 import { CodeBlock } from "@/components/ui/code-block";
